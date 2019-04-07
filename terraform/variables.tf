50569483ff96051337754083dd47d9ddee15376f
Paul <silverwoodp@btinternet.com>
2019-04-07 14:48:46 +0100

    Removed AWS keys so that the git repo can go public securely

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 # AWS Config
 
 variable "aws_access_key" {
-  default = "AKIASO45FBUTTQ2RYR7S"
+  default = # Redacted for security
 }
 
 variable "aws_secret_key" {
-  default = "nC5Khf90SmFv9q037Vip56i+BaAAaW6A0TD6J6xQ"
+  default = # Redacted for security
 }
 
 variable "aws_region" {
